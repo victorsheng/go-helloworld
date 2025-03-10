4898ea5cd1ce19d9925478ce3bfb0ce9b29b3019
shengsiyu <shengsiyu2014@xiaochuankeji.cn>
2025-03-10 11:47:14 +0800

    m

--- 2025/0306/select.go
+++ 2025/0306/select.go
@@ -13,10 +13,12 @@ func main() {
 	c2 := make(chan string)
 
 	go func() {
+		c1 <- "one"
 		time.Sleep(1 * time.Second)
 		c1 <- "one"
 	}()
 	go func() {
+		c2 <- "two"
 		time.Sleep(2 * time.Second)
 		c2 <- "two"
 	}()
